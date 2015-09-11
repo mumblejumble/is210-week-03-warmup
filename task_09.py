259f9fd2ad124e039c456f14226662ff44ef071b
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-09-11 05:02:30 +0000

    task9

--- task_09.py
+++ task_09.py
@@ -5,3 +5,11 @@
 THE_ANSWER_TO_EVERYTHING = 42
 
 INFINITE_IMPROBABILITY = 'browning motion'
+
+'''Line1 interpreter directive
+   line2 another part of interpreter directive
+   line3 kinda like a title
+   line4 empty
+   line5 IDK what'''
+import task_09
+help(task_09)
