43043887cfd0425937c5913b4a3720a76e77bb8c
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-09-11 05:04:05 +0000

    12

---
+++ task_12.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Task 10 """
+import decimal
+import fractions
+INTVAL = 1
+FLOATVAL = decimal.Decimal ('0.1' )
+DECVAL = decimal.Decimal ('0.1' )
+FRACVAL = fractions.Fraction ('1/10' )
+
+print INTVAL
+print FLOATVAL
+print DECVAL
+print FRACVAL
