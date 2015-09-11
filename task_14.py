1c9469be8ef5076f9d9ef954fd41c75bf9119050
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-09-11 05:04:12 +0000

    14

---
+++ task_14.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Task_14 """
+IS_TRUE = True
+IS_FALSE = False
+IS_NONE = None
+print IS_TRUE
+print IS_FALSE
+print IS_NONE
+INTEGER_EQUIV = IS_TRUE == 1 and IS_FALSE == 0
+print INTEGER_EQUIV
