41b6dce4147237a276f4a335cd41a83b621b1b8c
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-09-12 04:32:46 +0000

    9

--- task_09.py
+++ task_09.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""A computer that can build the question.Line1 interpreter directive
+   line2 another part of interpreter directive
+   line3 kinda like a title
+   line4 empty
+   line5 IDK what"""
+
+import task_09
 
 THE_ANSWER_TO_EVERYTHING = 42
 
 INFINITE_IMPROBABILITY = 'browning motion'
 
-'''Line1 interpreter directive
-   line2 another part of interpreter directive
-   line3 kinda like a title
-   line4 empty
-   line5 IDK what'''
-import task_09
 help(task_09)
