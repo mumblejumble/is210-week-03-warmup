6a8e1d25771ebb94aeb15c96d42ff5242bddaf4f
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-09-12 05:23:24 +0000

    12

--- task_12.py
+++ task_12.py
@@ -5,8 +5,8 @@ import decimal
 import fractions
 INTVAL = 1
 FLOATVAL = decimal.Decimal('0.1')
-DECVAL = 1 / 10.0
-FRACVAL = fractions.Fraction('1/10')
+DECVAL = decimal.Decimal('0.1')
+FRACVAL = fractions.Fraction(1, 10)
 
 print INTVAL
 print FLOATVAL
